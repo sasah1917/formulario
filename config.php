99fb4e878647f7c5f6ebd178c705864885ae533f
sasah1917 <92746412+sasah1917@users.noreply.github.com>
2022-11-14 15:23:24 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+    $dHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'formulario-italia'
+
+    $conexao = new mysqli($dHost,$dbUsername,$dbPassword,$dbName);
+
+    //if($conexao -> connect_errno)
+   // {
+    //    echo "Erro";
+   // }
+   // else
+   // {
+    //    echo "Conexao efetuada com sucesso";
+    //}
+?>
